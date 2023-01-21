d03c0695af0a8e4423795e88c0f9deb8dabb7b79
laurigurev <larigur@gmail.com>
2023-01-21 16:08:29 +0200

    refactored previous fix

--- writer.hpp
+++ writer.hpp
@@ -20,17 +20,28 @@ struct writer {
                 const table_header header = {id, QLOG_TABLE_TYPE_STATIC};
                 constexpr u32      arg_count = sizeof...(a);
                 const static_table table = {header, level, file.len, line, format.len, arg_count};
-
-                // const type_array<arg_count> arg_t = {a...};
+                const type_array<arg_count> arg_t = {a...};
 
                 ser.write(reinterpret_cast<const char*>(&table), sizeof(static_table));
                 ser.write(file._raw, file.len);
                 ser.write(format._raw, format.len);
+                ser.write(arg_t.types, sizeof(arg_t));
 
-                if constexpr (arg_count) {
-                        const type_array<arg_count> arg_t = {a...};
-                        ser.write(arg_t.types, sizeof(arg_t));
-                }
+                return id;
+        }
+        
+        template <u32 N, u32 M, typename... A>
+        constexpr u16 push_static(const u32 level, const cstring<N>& file, const u32 line, const cstring<M>& format)
+        {
+                const u16 id = unique_log_count;
+                unique_log_count++;
+
+                const table_header header = {id, QLOG_TABLE_TYPE_STATIC};
+                const static_table table = {header, level, file.len, line, format.len, 0};
+                
+                ser.write(reinterpret_cast<const char*>(&table), sizeof(static_table));
+                ser.write(file._raw, file.len);
+                ser.write(format._raw, format.len);
 
                 return id;
         }
@@ -40,18 +51,23 @@ struct writer {
         {
                 const table_header  header = {id, QLOG_TABLE_TYPE_DYNAMIC};
                 const dynamic_table table = {header};
-
-                constexpr u32 arg_count = sizeof...(a);
-                // const tuple<A...>   tuple = {a...};
+                const tuple<A...>   tuple = {a...};
 
                 // printf("qlog::writer::push_dynamic(...), sizeof(tuple) %llu\n", sizeof(tuple));
 
                 ser.write(reinterpret_cast<const char*>(&table), sizeof(dynamic_table));
+                ser.write(reinterpret_cast<const char*>(&tuple), sizeof(tuple));
+        }
+        
+        template <typename... A>
+        constexpr void push_dynamic(const u16 id)
+        {
+                const table_header  header = {id, QLOG_TABLE_TYPE_DYNAMIC};
+                const dynamic_table table = {header};
 
-                if constexpr (arg_count) {
-                        const tuple<A...> tuple = {a...};
-                        ser.write(reinterpret_cast<const char*>(&tuple), sizeof(tuple));
-                }
+                // printf("qlog::writer::push_dynamic(...), sizeof(tuple) %llu\n", sizeof(tuple));
+
+                ser.write(reinterpret_cast<const char*>(&table), sizeof(dynamic_table));
         }
 
         u64        unique_log_count;
